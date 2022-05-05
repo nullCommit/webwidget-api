8277c92e0fe231beb909bc0362be2fea46c49168
Allan <allangeek@gmail.com>
2022-05-05 16:09:20 -0300

    Feature: send automatic e-mail with feedback

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,21 @@
  */
 
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '07a26093469ba2',
+    pass: '63d73ea233ea56',
+  },
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -22,6 +32,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Allan Duarte <allangeek@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n'),
   });
 
   return res.status(201).json({ data: feedback });
