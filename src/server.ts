3fa4c4bc532ea3ada26d62b135a0f008adddeb48
Allan <allangeek@gmail.com>
2022-05-05 15:51:00 -0300

    Feature: create feedbacks route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
+/**
+ * GET - Buscar informação
+ * POST - Cadastrar informação
+ * PUT - Atualizar informação de uma entidade
+ * PATCH - Atualizar informação única de uma entidade
+ * DELETE - Deletar uma informação
+ */
+
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hello world');
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
